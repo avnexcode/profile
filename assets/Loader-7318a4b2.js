a6428c00bbf91c19ec935e76eb752b7d64ddef84
Muhammad Fauzi Nur Aziz <axnvee18@gmail.com>
2024-03-05 10:11:12 +0700

    Updates

--- assets/Loader-7318a4b2.js
+++
@@ -1,1 +0,0 @@
-import{j as i}from"./index-3f0d6dd9.js";function r(){return i.jsx(i.Fragment,{children:i.jsx("div",{id:"page",className:"bg-white dark:bg-slate-700",children:i.jsxs("div",{id:"container",children:[i.jsx("div",{id:"ring"}),i.jsx("div",{id:"ring"}),i.jsx("div",{id:"ring"}),i.jsx("div",{id:"ring"})]})})})}export{r as Loader};
